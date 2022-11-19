05aa64421d2ec2b4fa58805558c18559276e6d7d
HipsterOK <stero141@gmail.com>
2022-11-19 13:34:31 +0500

    init commit

---
+++ src/main/kotlin/com/porcupine/Application.kt
@@ -0,0 +1,17 @@
+package com.porcupine
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.porcupine.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
